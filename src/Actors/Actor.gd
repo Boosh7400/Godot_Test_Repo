c84cee9a6b96a65f95be3088e3aa9b479e37c771
Boosh7400 <daniil.bush74@proton.me>
2022-07-21 20:06:36 +0300

    Enemy added and polished

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -3,11 +3,8 @@ class_name Actor
 
 const FLOOR_NORMAL = Vector2.UP
 
-export var speed:= Vector2(800, 1000.0)
+export var speed:= Vector2(300, 1000.0)
 export var gravity:= 3000.0
 
 var _velocity:= Vector2.ZERO
 
-	
-#	_velocity.y > max(_velocity.y, speed.y)
-	
--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,11 +1,18 @@
 extends Actor
 
+export var stomp_impulse = 1000.0
+
 func _physics_process(delta: float) -> void:
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0 
 	var direction: = get_direction()
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
 	_velocity= move_and_slide(_velocity, FLOOR_NORMAL)
 
+func _on_EnemyDetector_area_entered(area) -> void:
+	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
+
+func _on_EnemyDetector_body_entered(body) -> void:
+	queue_free()
 
 func get_direction() -> Vector2:
 	return Vector2(
@@ -26,4 +33,9 @@ func calculate_move_velocity(
 		out.y = 0.0
 	if direction.y == -1.0:
 		out.y = speed.y * direction.y
+	return out
+
+func calculate_stomp_velocity(linear_velocity: Vector2, impulse: float) -> Vector2:
+	var out: = linear_velocity
+	out.y = -impulse
 	return out
